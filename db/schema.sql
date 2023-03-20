88b765594c2b5426788f73a16bfcbc8120cdbf6c
UserHAL-9000 <jonconnorrobinson94@gmail.com>
2023-03-20 01:38:37 -0700

    file structure and scheme

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+-- Drops the database if it already exists
+DROP DATABASE IF EXISTS employees_db;
+
+-- Create the DB "employee_db"
+CREATE DATABASE employees_db;
+
+-- Use the db for the rest of the script
+USE employees_db;
+
