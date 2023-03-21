6fc816d360c2c18c9821beea7d2d9f8c9f040992
UserHAL-9000 <jonconnorrobinson94@gmail.com>
2023-03-21 15:44:04 -0700

    schema and seeds file

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,29 @@ CREATE DATABASE employees_db;
 -- Use the db for the rest of the script
 USE employees_db;
 
+-- Create table department
+CREATE TABLE departments (
+    id int AUTO_INCREMENT NOT NULL,
+    name varchar(30) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+-- Create table roles
+CREATE TABLE roles (
+    id int AUTO_INCREMENT NOT NULL,
+    title varchar(30) NOT NULL,
+    salary DECIMAL(6, 0),
+    department_id int NOT NULL,
+    PRIMARY KEY(id)
+);
+
+-- Create table employee
+CREATE TABLE employees (
+    id int AUTO_INCREMENT NOT NULL,
+    first_name varchar(30) NOT NULL,
+    last_name varchar(30) NOT NULL,
+    role_id int NOT NULL,
+    manager_id int,
+    PRIMARY KEY(id)
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,29 @@
+-- Insert departments into the department table
+INSERT INTO departments (name)
+VALUES ("Sales"), ("Engineering"), ("Legal"), ("Finance");
+
+-- Insert roles into roles table
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Lead Salesperson", 80000, 1),
+    ("Junior Salesperson", 50000, 1),
+    ("Senior Software Engineer", 150000, 2),
+    ("Junior Software Engineer", 80000, 2),
+    ("Lawyer", 140000, 3),
+    ("Paralegal", 110000, 3),
+    ("Accountant", 100000, 4);
+
+    -- Insert employees into the employees table
+    INSERT INTO employees (first_name, last_name, role_id, manager_id)
+    VALUES ("Jack", "Sparrow", 1, null),
+    ("Hector", "Barbosa", 2, 1),
+    ("Carina", "Smyth", 2, 1),
+    ("Elizabeth", "Swann", 3, null),
+    ("Will", "Turner", 4, 3),
+    ("Sao", "Feng", 4, 3),
+    ("Weatherby", "Swann", 4, 3),
+    ("Davy", "Jones", 6, null),
+    ("Tia", "Dalma", 6, 5),
+    ("Henry", "Turner", 6, 5),
+    ("Joshamee", "Gibs", 7, null),
+    ("James", "Norington", 7, 7),
+    ("Armando", "Salazar", 7, 7);
